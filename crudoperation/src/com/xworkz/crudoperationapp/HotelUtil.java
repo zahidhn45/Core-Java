83eeef4ed36d89f87a6b7d64d806d3a4c8ff0723
Zahid <mdzahidhussain.xworkz@gmail.com>
2021-11-17 20:55:05 +0530

    Interface

--- crudoperation/src/com/xworkz/crudoperationapp/HotelUtil.java
+++
@@ -1,5 +0,0 @@
-package com.xworkz.crudoperationapp;
-
-public class HotelUtil {
-
-}
--- supermarket/src/com/xwork/supermarketapp/Grocery.java
+++
@@ -1,7 +0,0 @@
-package com.xwork.supermarketapp;
-
-public class Grocery {
-	public static void main(String[] args) {
-		System.out.println("Hello world");
-	}
-}
